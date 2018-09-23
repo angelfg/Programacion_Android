f98ae8fa434aaa39b1a640afaffd649c78635a55
Angel Daniel Fernández González <angelfg@cifprodolfoucha.es>
2018-09-23 13:42:20 +0200

    Versión usuario 1 Creada unha activity Main2Activity_Usuario_1

--- app/src/main/java/es/cifprodofloucha/angel/depuracion/exemplogit/Main2Activity_Usuario_1.java
+++ app/src/main/java/es/cifprodofloucha/angel/depuracion/exemplogit/Main2Activity_Usuario_1.java
@@ -3,11 +3,11 @@ package es.cifprodofloucha.angel.depuracion.exemplogit;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main2Activity_Usuario_2 extends Activity {
+public class Main2Activity_Usuario_1 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2__usuario_2);
+        setContentView(R.layout.activity_main2__usuario_1);
     }
 }
