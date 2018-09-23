bfaac41411caaaac2d03da0deb304636737f8c46
Usuario 2 <petergs723@gmail.com>
2018-09-23 14:17:45 +0200

    Unido a master.

--- app/src/main/java/es/cifprodofloucha/angel/depuracion/exemplogit/Main2Activity_Usuario_1.java
+++ app/src/main/java/es/cifprodofloucha/angel/depuracion/exemplogit/Main2Activity_Usuario_1.java
@@ -2,6 +2,7 @@ package es.cifprodofloucha.angel.depuracion.exemplogit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity_Usuario_1 extends Activity {
 
@@ -9,5 +10,7 @@ public class Main2Activity_Usuario_1 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2__usuario_1);
+
+        Toast.makeText(getApplicationContext(),"Ola",Toast.LENGTH_LONG).show();
     }
 }
