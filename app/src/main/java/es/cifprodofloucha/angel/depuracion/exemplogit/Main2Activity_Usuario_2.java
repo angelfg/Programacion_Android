f7ba0eb3b2df2f0fb1d4641859fc1b6a38f5a815
Usuario 2 <petergs723@gmail.com>
2018-09-23 13:09:33 +0200

    Creada nueva actividad Main2Activity_User_2

---
+++ app/src/main/java/es/cifprodofloucha/angel/depuracion/exemplogit/Main2Activity_Usuario_2.java
@@ -0,0 +1,13 @@
+package es.cifprodofloucha.angel.depuracion.exemplogit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main2Activity_Usuario_2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2__usuario_2);
+    }
+}
